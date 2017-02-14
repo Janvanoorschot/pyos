ee7389273a23509d8dc5b431711bbf672c2cab10
Jan van Oorschot <janvanoorschot@gmail.com>
2017-02-14 11:07:45 +0100

    cleanup.

--- bin/runps.py
+++ bin/runps.py
@@ -5,4 +5,7 @@ output = subprocess.check_output(["ps", "ax", "-o", "pid="])
 
 for line in output.split('\n'):
     pid = int(line)
-    print("pid: " + pid)
+    if pid:
+        print("pid: " + str(pid))
+    else:
+        print("invalid line: " + line)
