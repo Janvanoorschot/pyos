8e3cd934e71249d63d92d06522bede2056a454d4
Jan van Oorschot <janvanoorschot@gmail.com>
2017-02-14 11:19:04 +0100

    cleanup.

--- bin/runps.py
+++ bin/runps.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python
 import subprocess
 
+# collect pids
+pids = []
 output = subprocess.check_output(["ps", "ax", "-o", "pid="])
-
 for line in output.split('\n'):
     try:
         pid = int(line)
-        print("pid: " + str(pid))
+        pids.append(pid)
     except:
-        print("invalid line: " + line)
+        pass
+
+# per pid, count threads
+for pid in pids:
+    spids=[]
+    output = subprocess.check_output(["ps", "-e", "-T", "-q", str(pid), "-o", "spid="])
+    for line in output.split('\n'):
+        try:
+            spid = int(line)
+            spids.append(spid)
+        except:
+            pass
+    if spids.len() > 1:
+        print(str(pid))
